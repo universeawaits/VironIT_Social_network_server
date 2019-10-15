8bc169c710d9919ecc458f789532bd89117c7e63
River Endless <myowngollum@gmail.com>
2019-10-16 01:42:25 +0300

    Merged

--- VironIT_Social_network_server/VironIT_Social_network_server.WEB/Startup.cs
+++ VironIT_Social_network_server/VironIT_Social_network_server.WEB/Startup.cs
@@ -1,16 +1,27 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
+
+using AutoMapper;
+
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
+
+using VironIT_Social_network_server.BLL;
+using VironIT_Social_network_server.BLL.Services;
+using VironIT_Social_network_server.BLL.Services.Interface;
 using VironIT_Social_network_server.WEB.Identity;
 using VironIT_Social_network_server.WEB.Identity.JWT;
+using VironIT_Social_network_server.WEB.IdentityProvider;
 
 
 namespace VironIT_Social_network_server.WEB
@@ -27,9 +38,13 @@ namespace VironIT_Social_network_server.WEB
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<IdentityContext>(
-                options => options.UseNpgsql(Configuration.GetConnectionString("UsersConnection")));
+                options => 
+                { 
+                    options.UseNpgsql(Configuration.GetConnectionString("UsersConnection")); 
+                });
             services.AddIdentity<User, IdentityRole>()
-                .AddEntityFrameworkStores<IdentityContext>();
+                .AddEntityFrameworkStores<IdentityContext>()
+                .AddDefaultTokenProviders();
             services.Configure<IdentityOptions>(
                 options =>
                 {
@@ -40,6 +55,13 @@ namespace VironIT_Social_network_server.WEB
                     options.Password.RequireLowercase = false;
 
                     options.User.RequireUniqueEmail = true;
+                });
+            services.AddDbContext<ImageContext>(
+                options =>
+                {
+                    options.UseNpgsql(
+                        Configuration.GetConnectionString("ImagesConnection"),
+                        b => b.MigrationsAssembly("VironIT_Social_network_server.WEB"));
                 });
 
             services.AddCors();
@@ -59,7 +81,7 @@ namespace VironIT_Social_network_server.WEB
                         ValidAudience = JwtOptions.Audience,
                         ValidateLifetime = true,
                         IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(JwtOptions.Secret)),
-                        ValidateIssuerSigningKey = true,
+                        ValidateIssuerSigningKey = true
                     };
                     options.Events = new JwtBearerEvents
                     {
@@ -74,12 +96,21 @@ namespace VironIT_Social_network_server.WEB
                                 context.Token = token;
                             }
                             return Task.CompletedTask;
+                        },
+                        OnAuthenticationFailed = context =>
+                        {
+                            context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                            return Task.CompletedTask;
                         }
                     };
                 }
             );
 
             services.AddSignalR();
+            services.AddAutoMapper(typeof(BLLMapperProfile));
+            services.AddTransient<IImageService, ImageService>();
+            services.AddTransient<IEmailService, EmailService>();
+            services.AddScoped<IUnitOfWork<ImageContext>, UnitOfWork<ImageContext>>();
             services.AddControllers();
         }
 
@@ -94,10 +125,17 @@ namespace VironIT_Social_network_server.WEB
                 .AllowAnyOrigin()
                 .AllowAnyMethod()
                 .AllowAnyHeader());
+            app.UseAuthentication();
             app.UseHttpsRedirection();
             app.UseDefaultFiles();
             app.UseStaticFiles();
-            app.UseAuthentication();
+            app.Use(async (context, next) =>
+            {
+                if (context.Response.StatusCode != StatusCodes.Status401Unauthorized)
+                {
+                    await next.Invoke();
+                }
+            });
             app.UseRouting();
             app.UseEndpoints(builder =>
             {
